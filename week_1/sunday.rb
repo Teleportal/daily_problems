a80a703e6a2851bc59f1018d5aa32bab0e999419
Teleportal <joshthegreenberg@gmail.com>
2018-01-07 16:58:00 -0600

    created sunday.rb, and did the daily problem with both the .map method and the .map! method

---
+++ week_1/sunday.rb
@@ -0,0 +1,17 @@
+numbers = [1, 2, 4, 2]
+doubled_numbers = []
+numbers.each do |number|
+  doubled_numbers << number * 2
+end
+p doubled_numbers
+
+puts "++++++++++++"
+
+numbers = [1, 2, 4, 2]
+doubled_numbers = numbers.map { |number| number * 2}
+p doubled_numbers
+puts "++++++++++++"
+
+numbers = [1, 2, 4, 2]
+numbers.map! { |number| number * 2} 
+p numbers
